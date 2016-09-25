21385da3b32025c135acdbc5e56147c87ac11480
joaopaulo164 <joaopaulo164@hotmail.com>
2016-09-25 14:56:51 -0300

    first commit

---
+++ src/br/com/devmedia/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package br.com.devmedia.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/bemvindo")
+	public ModelAndView bemVindo(Model model){
+		model.addAttribute("bemvindo" , "Olha só que facil dizer bem vindo");
+		return new ModelAndView("bemvindo");
+	}
+
+}
